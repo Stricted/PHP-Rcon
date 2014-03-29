48e9024acb38b9d6bb88bcb64c15fa80b3fb9326
Stricted <info@stricted.de>
2014-03-29 22:35:26 +0100

    add COD5

---
+++ games/COD5.class.php
@@ -0,0 +1,12 @@
+<?php
+require_once("games/COD4.class.php");
+
+/**
+ * Call of Duty 5: World at War 
+ *
+ * @author      Jan Altensen (Stricted)
+ * @copyright   2013-2014 Jan Altensen (Stricted)
+ * @license     GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class CODWAW extends COD4 {
+}
